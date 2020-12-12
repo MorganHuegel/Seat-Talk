55a6866a7a7891bd79185bf6002131a012d964d5
Morgan Huegel <morgan@Morgans-MacBook-Pro.local>
2020-12-12 12:11:01 -0500

    initialized app

---
+++ components/VideoMain/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class VideoMain extends React.Component {
+  render() {
+    return <p>{this.props.sessionId}</p>;
+  }
+}
---
+++ pages/[session].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import VideoMain from "../components/VideoMain";
+
+function Session() {
+  const router = useRouter();
+  const sessionId = router.query.session;
+
+  return (
+    <>
+      <p>{sessionId}</p>
+      <VideoMain sessionId={sessionId} />
+    </>
+  );
+}
+
+export default Session;
