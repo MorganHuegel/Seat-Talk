01e3791af9e9d4c758a9c775b909c632bbd43f66
Morgan Huegel <morgan@Morgans-MacBook-Pro.local>
2020-12-13 20:14:26 -0500

    added align buttons to video player

--- components/VideoMain/OwnVideo/index.js
+++ components/VideoMain/OwnVideo/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from '../../../styles/Components/VideoMain/OwnVideo/OwnVideo.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAlignRight, faAlignLeft, faAlignCenter } from '@fortawesome/free-solid-svg-icons'
 
 const OwnVideo = React.forwardRef((props, ref) => {
+    const [justifyVideo, setJustifyVideo] = useState('center')
+
     return (
-        <div className={style.videoContainer}>
-            <video ref={ref} />
+        <div className={style.videoContainer} style={{ justifyContent: justifyVideo }}>
+            <div className={style.videoPlayer}>
+                <div className={style.alignButtonContainer}>
+                    <button type="button" onClick={() => setJustifyVideo('flex-start')}>
+                        <FontAwesomeIcon icon={faAlignLeft} />
+                    </button>
+                    <button type="button" onClick={() => setJustifyVideo('center')}>
+                        <FontAwesomeIcon icon={faAlignCenter} />
+                    </button>
+                    <button type="button" onClick={() => setJustifyVideo('flex-end')}>
+                        <FontAwesomeIcon icon={faAlignRight} />
+                    </button>
+                </div>
+                <video ref={ref} />
+            </div>
         </div>
     )
 })
