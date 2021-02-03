06241711b6426c517070d9d06fe9db8f813435f5
Morgan Huegel <mhue2910@gmail.com>
2021-02-03 01:03:19 -0500

    form for display name

--- components/SecureWrapper/index.js
+++ components/SecureWrapper/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import style from '../../styles/Components/SecureWrapper/SecureWrapper.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
@@ -6,24 +6,65 @@ import Link from 'next/link'
 const SecureWrapper = (props) => {
     const currentPath = useRouter().pathname
     const [acceptJoin, setAcceptJoin] = useState(false)
+    const [requiredMessage, setRequiredMessage] = useState(false)
+    const [displayName, setDisplayName] = useState('')
+    const displayNameInput = useRef()
+
+    useEffect(() => {
+        // must put this code in useEffect because window is not defined when server-rendered
+        const webToken = window.localStorage.getItem('authToken')
+        if (webToken) {
+            setDisplayName('fooBar') // decode web token
+        }
+    }, [])
+
+    function handleClickJoin() {
+        let name = displayNameInput.current.value
+        if (!name) {
+            setRequiredMessage(true)
+        }
+
+        // get JSON Web Token and store in local storage
+        setAcceptJoin(true)
+    }
 
     return (
         <>
             {currentPath === '/[room]' && !acceptJoin && (
                 <div className={style.modalContainer}>
                     <div className={style.formContainer}>
-                        <h2 className={style.formHeader}>Welcome!</h2>
+                        {displayName ? (
+                            <h2 className={style.formHeader}>Welcome, {displayName}!</h2>
+                        ) : (
+                            <>
+                                <h2 className={style.formHeader}>Welcome!</h2>
+                                <label htmlFor="display-name">
+                                    Display Name:{' '}
+                                    {requiredMessage && (
+                                        <span className={style.requiredMessage}>*required</span>
+                                    )}
+                                </label>
+                                <input
+                                    className={`${style.formInput} ${
+                                        requiredMessage && style.required
+                                    }`}
+                                    name="display-name"
+                                    ref={displayNameInput}
+                                    onChange={() => setRequiredMessage(false)}
+                                />
+                            </>
+                        )}
                         <div className={style.buttonContainer}>
-                            <Link href="/">
-                                <a className={style.homeLink}>Home</a>
-                            </Link>
                             <button
                                 type="button"
                                 className={style.joinButton}
-                                onClick={() => setAcceptJoin(true)}
+                                onClick={handleClickJoin}
                             >
                                 Join Room
                             </button>
+                            <Link href="/">
+                                <a className={style.homeLink}>Return Home</a>
+                            </Link>
                         </div>
                     </div>
                 </div>
