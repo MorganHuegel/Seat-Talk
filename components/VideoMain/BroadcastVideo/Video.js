d1d4881a286b2b897284abb10c7de93225f2d7e8
Morgan Huegel <mhue2910@gmail.com>
2021-02-27 01:18:18 -0500

    display name on broadcast video grid tile

--- components/VideoMain/BroadcastVideo/Video.js
+++ components/VideoMain/BroadcastVideo/Video.js
@@ -45,52 +45,10 @@ const Video = (props) => {
 
     return (
         <div className={style.videoContainer} style={styles}>
+            <p className={style.displayName}>
+                {isScreenShare ? client.display_name + "'s Screen" : client.display_name}
+            </p>
             <video ref={videoNode} />
-        </div>
-    )
-    let [currentVideoTrackId, setCurrentVideoTrackId] = useState(null)
-
-    useEffect(() => {
-        // remove tracks that are no longer available
-        if (!availableTracks.length) {
-            ref.current.srcObject = null
-        }
-        let stream = ref.current.srcObject
-        if (stream) {
-            stream.getTracks().forEach((track) => {
-                if (!availableTracks.some((t) => t.id === track.id)) {
-                    track.stop()
-                    stream.removeTrack(track)
-                }
-            })
-        }
-
-        if (!currentVideoTrackId || !availableTracks.find((t) => t.id === currentVideoTrackId)) {
-            let videoTrack = availableTracks.find((t) => t.kind === 'video')
-            setCurrentVideoTrackId(videoTrack ? videoTrack.id : null)
-        }
-    }, [availableTracks])
-
-    // async function addTracks() {
-    //     const stream = new MediaStream()
-    //     availableTracks.forEach((t) => {
-    //         if (t.kind === 'audio' || t.id === currentVideoTrackId) {
-    //             stream.addTrack(t)
-    //         }
-    //     })
-    //     ref.current.srcObject = stream
-    //     if (ref.current.paused) {
-    //         await ref.current.play()
-    //     }
-    // }
-
-    if (ref.current && availableTracks.length) {
-        addTracks()
-    }
-
-    return (
-        <div className={style.videoContainer}>
-            <video ref={ref} id="broadcast-video" />
         </div>
     )
 }
