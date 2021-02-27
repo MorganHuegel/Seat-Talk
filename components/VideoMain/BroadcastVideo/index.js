2333d1a8e73b1d61e33086d6943fbb02515966a7
Morgan Huegel <mhue2910@gmail.com>
2021-02-27 00:10:32 -0500

    broadcast video grid adjusts on resize

--- components/VideoMain/BroadcastVideo/index.js
+++ components/VideoMain/BroadcastVideo/index.js
@@ -6,6 +6,13 @@ import Video from './Video'
 const BroadcastVideo = (props) => {
     const { otherClientsInRoom, availableTracks } = props
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    useEffect(() => {
+        function handleResize(e) {
+            setWindowWidth(e.currentTarget.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    })
 
     let clientCount = otherClientsInRoom.length
     let styles
@@ -15,6 +22,10 @@ const BroadcastVideo = (props) => {
         styles = { width: '100%', height: '50%' }
     } else if (clientCount <= 4) {
         styles = { width: '50%', height: '50%' }
+    } else if (windowWidth > 1200) {
+        styles = { width: '25%', height: '33.33%' }
+    } else if (windowWidth > 900) {
+        styles = { width: '33.33%', height: '33.33%' }
     } else {
         styles = { width: '50%', height: '33.33%' }
     }
