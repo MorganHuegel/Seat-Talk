17352ac272df9af6575a52f2d373fb8ea5b83742
Morgan Huegel <mhue2910@gmail.com>
2021-03-28 00:04:49 -0400

    textarea and submits on shift+enter

--- components/Chat/index.js
+++ components/Chat/index.js
@@ -3,13 +3,23 @@ import style from '../../styles/Components/Chat/Chat.module.css'
 import { ClapButton, CodeButton } from '../Buttons'
 
 const Chat = (props) => {
+    const chatForm = useRef(null)
     const chatInput = useRef(null)
+    const codeInput = useRef(null)
     const [isCode, setIsCode] = useState(false)
 
     function handleChatSubmit(e) {
-        e.preventDefault()
+        if (e) e.preventDefault()
         console.log('chatIinput: ', chatInput.current.value)
+        console.log('codeInput: ', codeInput.current.value)
         chatInput.current.value = ''
+        codeInput.current.value = ''
+    }
+
+    function checkForSubmit(e) {
+        if (e.code === 'Enter' && e.shiftKey) {
+            handleChatSubmit()
+        }
     }
 
     function handleClap() {
@@ -23,8 +33,16 @@ const Chat = (props) => {
     return (
         <div className={style.container}>
             <div>Chat stuff</div>
-            <form onSubmit={handleChatSubmit}>
-                <input ref={chatInput} type="text" className={style.input} />
+            <form onSubmit={handleChatSubmit} ref={chatForm}>
+                <div className={style.codeInput} style={{ display: isCode ? 'block' : 'none' }}>
+                    <textarea ref={codeInput} onKeyDown={checkForSubmit} />
+                </div>
+                <input
+                    ref={chatInput}
+                    type="text"
+                    className={style.input}
+                    style={{ display: isCode ? 'none' : 'inline-block' }}
+                />
                 <div className={style.btnContainer}>
                     <button>Emoji</button>
                     <CodeButton handleClick={handleClickCode} isCode={isCode} />
